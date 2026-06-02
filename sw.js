7999747cb7e7d92681539347dc63118bfce9a85a
jihooni2000 <88198149+jihooni2000@users.noreply.github.com>
2026-06-02 09:55:34 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'janghak-punchlist-202606020040';
+const CACHE_NAME = 'janghak-punchlist-202606020054';
 const ASSETS = [
   '/quality-inspection/',
   '/quality-inspection/index.html',
